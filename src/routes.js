4b0e4850d6f5c648e28e0b3c4b38ee3f0f7680d9
Willian <wsilvadev@gmail.com>
2020-03-25 12:18:05 -0300

    feat(crypto): generating an encryption id

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,31 @@
-const express = require('express');
+const express = require("express");
+// crypto é para criptografia mas usaremos para criar uma string aleatória
+const cripto = require("crypto");
+const connection = require("./database/index");
 
 const routes = express.Router();
 
-routes.post("/users", (req, res) => {
-    res.json(req.body);
-})
+routes.get("/ongs", async (req, res) => {
+  const ongs = await connection("ongs").select("*");
+
+  return res.json(ongs);
+});
+routes.post("/ongs", async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  // id vai gerar quatro bytes de caracteres aleatórios
+  const id = cripto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return res.json({ id });
+});
 module.exports = routes;
+`
